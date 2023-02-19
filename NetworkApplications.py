9b0543354afa756f05cf9ac9ceafe5a14aa6809b
THELAXON <l.thavarajah@lancaster.ac.uk>
2023-02-19 19:34:09 +0000

    webserver works

--- NetworkApplications.py
+++ NetworkApplications.py
@@ -388,7 +388,7 @@ class ParisTraceroute(NetworkApplication):
 
 class WebServer(NetworkApplication):
     
-    def handleRequest(tcpSocket):
+    def handleRequest(self, tcpSocket):
         request = tcpSocket.recv(1024).decode('utf-8')
         path = request.split(' ')[1]
         
@@ -411,32 +411,11 @@ class WebServer(NetworkApplication):
         try:
             while True:
                 connSocket, addr = serverSocket.accept()
-                self.handleRequest(connSocket)
+                self.handleRequest(connSocket)  # pass connSocket as second argument
         finally:
             serverSocket.close()
 
     
-
-class WebServer(NetworkApplication):
-
-    def handleRequest(tcpSocket):
-        # 1. Receive request message from the client on connection socket
-        # 2. Extract the path of the requested object from the message (second part of the HTTP header)
-        # 3. Read the corresponding file from disk
-        # 4. Store in temporary buffer
-        # 5. Send the correct HTTP response error
-        # 6. Send the content of the file to the socket
-        # 7. Close the connection socket
-        pass
-
-    def __init__(self, args):
-        print('Web Server starting on port: %i...' % (args.port))
-        # 1. Create server socket
-        # 2. Bind the server socket to server address and server port
-        # 3. Continuously listen for connections to server socket
-        # 4. When a connection is accepted, call handleRequest function, passing new connection socket (see https://docs.python.org/3/library/socket.html#socket.socket.accept)
-        # 5. Close server socket
-
 
 class Proxy(NetworkApplication):
 
